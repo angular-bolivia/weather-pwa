0bd992c2ffbc29a916e4012c1e533b2b361d216d
luixaviles <luis.avilesz@gmail.com>
2017-07-03 18:31:06 -0400

    Adding material module and main toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from '@angular/core';
+
+import { MdToolbarModule, MdIconModule, MdButtonModule } from "@angular/material";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MdToolbarModule,
+    MdIconModule,
+    MdButtonModule,
     AppRoutingModule
   ],
   providers: [],
