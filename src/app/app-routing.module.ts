8d337a24d0735a6e643445cca190e532fb560567
Mauricio Arce <mauricio.arce.mat@gmail.com>
2017-11-15 11:42:08 -0400

    Angular was updated to version 5 and service workers were added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,11 @@ import { HomeComponent } from "./home/home.component";
 const routes: Routes = [
   {
     path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
     component: HomeComponent
   }
 ];
